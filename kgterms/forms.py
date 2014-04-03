f2479193726897ca584d08e363c66928b37365ff
Kieran Spear <kispear@gmail.com>
2014-04-03 14:22:36 +1100

    Fix name of UserAgreed in form

--- kgterms/forms.py
+++ kgterms/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import ApplicantAgreed
+from .models import UserAgreed
 
 
 class AgreeToTermsForm(forms.ModelForm):
     i_agree = forms.BooleanField(initial=False)
 
     class Meta:
-        model = ApplicantAgreed
+        model = UserAgreed
 
     def __init__(self, *args, **kwargs):
         super(AgreeToTermsForm, self).__init__(*args, **kwargs)
